8821c3c1f861a4a2d1d8a6347a791ed831579ea2
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-12-09 10:25:36 +0700

    Fix error in settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from os.path import exists
 import environ
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
