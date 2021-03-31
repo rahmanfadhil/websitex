6fc7af010584d04e0a75ff6e1e6cdadc90869700
Rahman Fadhil <rhmnfadhil@gmail.com>
2021-03-31 10:52:44 +0700

    Add Permalinkable abstract model

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.db.models.query_utils import Q
+from django.utils import timezone
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
+from apps.core.utils import unique_slugify
 
 
 class Authorable(models.Model):
@@ -6,7 +11,7 @@ class Authorable(models.Model):
     Model can be owned by a registered user.
     """
 
-    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    author = models.ForeignKey("users.User", on_delete=models.SET_NULL, null=True)
 
     class Meta:
         abstract = True
@@ -20,8 +25,26 @@ class Timestampable(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    @property
+    def created_since(self) -> str:
+        return naturaltime(self.created_at)
+
+    @property
+    def updated_since(self) -> str:
+        return naturaltime(self.created_at)
+
     class Meta:
         abstract = True
+
+
+class PublishableQuerySet(models.QuerySet):
+    def published(self):
+        return self.filter(published_at__lte=timezone.now())
+
+    def draft(self):
+        return self.filter(
+            Q(published_at__isnull=True) | Q(published_at__gt=timezone.now())
+        )
 
 
 class Publishable(models.Model):
@@ -32,8 +55,45 @@ class Publishable(models.Model):
     published_at = models.DateTimeField(null=True, blank=True)
 
     @property
-    def is_published(self) -> bool:
-        return self.published_at is not None
+    def published_since(self) -> str:
+        return naturaltime(self.published_at)
+
+    def publish_on(self, datetime=None):
+        """
+        Publish record at a certain time.
+        """
+        if not datetime:
+            datetime = timezone.now()
+        elif timezone.is_naive(datetime):
+            datetime = timezone.make_aware(datetime)
+        self.publish_date = datetime
+        self.save()
+
+    @property
+    def is_published(self):
+        """
+        Determine if the record is published or not.
+        """
+        return self.publish_date < timezone.now()
+
+    class Meta:
+        abstract = True
+
+
+class Permalinkable(models.Model):
+    """
+    Generate an SEO friendly slug field.
+    """
+
+    slug = models.SlugField(unique=True)
+
+    def get_slug_source(self) -> str:
+        raise NotImplemented
+
+    def save(self, *args, **kwargs) -> str:
+        if not self.slug:
+            self.slug = unique_slugify(self.__class__, self.get_slug_source())
+        return super().save(*args, **kwargs)
 
     class Meta:
         abstract = True
