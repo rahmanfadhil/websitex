cf74ae77fab667bb32c67c2de2a74fc2f0bac3be
Rahman Fadhil <rhmnfadhil@gmail.com>
2021-04-04 23:10:57 +0700

    Separate js plugin initialization to another file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,3 @@
-import { Toast } from "bootstrap";
-import feather from "feather-icons";
-import flatpickr from "flatpickr";
-import Cleave from "cleave.js";
-import "trix";
+import "./plugins";
 
 import "../scss/main.scss";
-
-feather.replace({ width: "1em", height: "1em", "stroke-width": 2 });
-
-for (const el of document.querySelectorAll(".toast")) {
-  new Toast(el).show();
-}
-
-flatpickr("input.datetimepickerinput");
-
-new Cleave(".priceinput", {
-  numeral: true,
-  numeralThousandsGroupStyle: "thousand",
-});
