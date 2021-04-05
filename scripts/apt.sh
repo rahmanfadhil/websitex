59f7e08322b86e8b9509174d951eccad62c2eb4b
Rahman Fadhil <rhmnfadhil@gmail.com>
2021-04-05 08:54:03 +0700

    Add watchdog to auto reload celery in development

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -8,6 +8,9 @@ apt-get install -y build-essential
 # psycopg2 dependencies
 apt-get install -y libpq-dev
 
+# watchdog dependencies
+apt-get install -y libyaml-dev
+
 # python-magic dependencies
 apt-get install -y libmagic1
 
