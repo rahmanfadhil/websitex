17d87f13e0680895145cbbe5f1f044160db8a9ed
Rahman Fadhil <rhmnfadhil@gmail.com>
2021-04-12 21:51:36 +0700

    Fix error in full_url and active_link template tag when the template context has no request attribute

--- apps/core/templatetags/core.py
+++ apps/core/templatetags/core.py
@@ -8,7 +8,9 @@ def full_url(context, url):
     """
     Build a full url with domain and scheme (http or https).
     """
-    return context.request.build_absolute_uri(url)
+    return (
+        context.request.build_absolute_uri(url) if hasattr(context, "context") else url
+    )
 
 
 @register.simple_tag(takes_context=True)
@@ -16,4 +18,9 @@ def active_link(context, url_name):
     """
     Returns nav link active class if the current page is active.
     """
-    return "active" if context.request.resolver_match.url_name == url_name else ""
+    return (
+        "active"
+        if hasattr(context, "context")
+        and context.request.resolver_match.url_name == url_name
+        else ""
+    )
