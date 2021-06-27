df3a006a00df26858cee36a7baece205a06ac739
Rahman Fadhil <rhmnfadhil@gmail.com>
2021-06-28 06:23:32 +0700

    Update active_link template tag to use namespace

--- apps/core/templatetags/core.py
+++ apps/core/templatetags/core.py
@@ -1,10 +1,11 @@
 from django import template
+from django.urls import resolve
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def full_url(context, url):
+def full_url(context, url) -> str:
     """
     Build a full url with domain and scheme (http or https).
     """
@@ -14,11 +15,16 @@ def full_url(context, url):
 
 
 @register.simple_tag(takes_context=True)
-def active_link(context, *url_name):
+def active_link(context, *view_names) -> str:
     """
     Returns nav link active class if the current page is active.
     """
     if hasattr(context, "request"):
-        if context.request.resolver_match.url_name in url_name:
-            return "active"
+        for view_name in view_names:
+            try:
+                match = resolve(context.request.path_info)
+                if match.view_name == view_name:
+                    return "active"
+            except:
+                pass
     return ""
