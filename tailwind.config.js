decdeb13abc801bb6201dd7688dc7fb220738cb4
Rahman Fadhil <rhmnfadhil@gmail.com>
2021-12-14 17:25:40 +0700

    Upgrade to Tailwind 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,16 @@
 module.exports = {
-  mode: "jit",
-  purge: [
+  content: [
     "./templates/**/*.html",
     "./assets/**/*.{js,jsx,ts,tsx,css}",
     "./apps/**/*.py",
     "./config/**/*.py",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         sans: ["LexendVariable", "sans-serif"],
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
