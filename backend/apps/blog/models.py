7c1cb440b304055718131361726a31a419c6285b
Rahman Fadhil <rhmnfadhil@gmail.com>
2022-10-08 15:48:32 +0700

    Add missing pytest dependency

--- backend/apps/blog/models.py
+++ backend/apps/blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
-from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 
 
@@ -38,7 +37,7 @@ class BlogPage(Page):
 
     promote_panels = [
         MultiFieldPanel(Page.promote_panels, "Common page configuration"),
-        ImageChooserPanel("thumbnail"),
+        FieldPanel("thumbnail"),
     ]
 
     class Meta:
